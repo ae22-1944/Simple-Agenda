3893ef7317f1ef2fa97426ddfc6f7f52779abf45
Adán Estévez <adanescollante@gmail.com>
2024-03-17 18:23:35 -0400

    Revert "Modificación de método"
    This reverts commit 09a6512c660f08bc265f9c19afc244b35274cd60.

--- src/SimpleAgenda.java
+++ src/SimpleAgenda.java
@@ -1,33 +1,7 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.util.Scanner;
 
 public class SimpleAgenda {
     private static String[] contactos = new String[10];
-
-    public static void generarArchivo() {
-        File f = new File("contactos.txt");
-
-        try {
-            if (f.createNewFile()) {
-                System.out.println("El archivo fue creado exitosamente");
-            } else {
-                System.out.println("Un archivo con este nombre ya existe en el directorio");
-            }
-
-            FileWriter escritor = new FileWriter(f);
-            escritor.write("contacto,telefono\n" +
-                    "Adan,8098551212\n" +
-                    "Enmanuel,8294118787\n" +
-                    "Raider,8097410032\n" +
-                    "Roger,8095554141\n");
-            escritor.close();
-
-        } catch (Exception e) {
-            System.err.println(e);
-        }
-    }
 
     public static void agregarContacto() {
         Scanner sc = new Scanner(System.in);
@@ -62,55 +36,17 @@ public class SimpleAgenda {
     }
 
     public static void mostrarContacto() {
-        File archivo = new File("contactos.txt");
-        Scanner leerConsola = new Scanner(System.in);
-        Scanner leerArchivo = null;
-        boolean contactoExiste = false;
-
-        System.out.println("Ingrese contacto a buscar");
-        String contactoBuscando = leerConsola.nextLine();
-
-        try {
-            leerArchivo = new Scanner(archivo);
-
-            // Skip the first line
-            if (leerArchivo.hasNextLine()) {
-                leerArchivo.nextLine();
-            }
-
-            System.out.println("Mostrando contactos...");
-
-            while (leerArchivo.hasNextLine()) {
-                String linea = leerArchivo.nextLine();
-                System.out.println(linea);
-
-                String[] partes = linea.split(",");
-
-                if (partes[0].equalsIgnoreCase(contactoBuscando)) {
-                    contactoExiste = true;
-                }
-            }
-
-            if (contactoExiste) {
-                System.out.println("El contacto " + contactoBuscando + " existe");
-            } else {
-                System.out.println("No existen registros de este contacto");
-            }
-
-        } catch (FileNotFoundException e) {
-            System.err.println(e);
-        } finally {
-            if (leerArchivo != null) {
-                leerConsola.close();
-                leerArchivo.close();
+        System.out.println("Mostrando contactos...");
+        for (String c : contactos) {
+            if (c != null) {
+                System.out.println(c);
             }
         }
     }
 
     public static void main(String[] args) {
-        generarArchivo();
-        mostrarContacto();
         agregarContacto();
         removerContacto();
+        mostrarContacto();
     }
 }
